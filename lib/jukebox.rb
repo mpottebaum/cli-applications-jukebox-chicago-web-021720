c4ee81649d915f4c984cbf86ba445c2ea56d7261
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-31 01:01:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,6 @@
 def help(user_input)
   puts "I accept the following commands:"
   puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
   puts ""
 end
